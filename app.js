68a976f967af8a36062a39da92ebb4c37d069975
Nihanshu Purohit <npurohit@purdue.edu>
2016-11-24 18:12:17 -0600

    MongoDb is connected.

--- app.js
+++ app.js
@@ -21,6 +21,42 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+/* mongo db setup */
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+ 
+// Connection URL 
+var url = 'mongodb://d8354849-9635-41a3-b1cc-5f70ba99090e:5452eaa4-d5b3-4c8f-8a08-a4273b51f377@23.246.199.101:10021/db';
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+  insertDocuments(db, function() {
+    db.close();
+  });
+});
+/* setup ends */
+
+/* testing db */
+var insertDocuments = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('documents');
+  // Insert some documents 
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+}
+/* testing ends */
+
+app.get('/nihanshu', function (req, res) {
+  res.send('index.html')
+})
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
